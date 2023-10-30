64b69b1af7b149fd171e26278bfbeccd39465127
Dre Tay <andretaylor681@gmail.com>
2023-10-30 15:22:35 -0400

    added in the starter code that i completed

---
+++ Develop/models/index.js
@@ -0,0 +1,33 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category);
+// Categories have many Products
+Category.hasMany(Product);
+// Products belongToMany Tags (through ProductTag)
+Product.belongToMany( Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as:'product_tags'
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongToMany( Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_products'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
